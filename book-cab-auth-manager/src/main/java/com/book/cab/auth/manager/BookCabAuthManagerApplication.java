045c6ef450cc05d4ca49b7600fdb483f52197791
Sunil Bhambani <sunil_bhambani@persistent.co.in>
2019-10-07 12:10:56 +0530

    Added schema and data sql files

--- book-cab-auth-manager/src/main/java/com/book/cab/auth/manager/BookCabAuthManagerApplication.java
+++ book-cab-auth-manager/src/main/java/com/book/cab/auth/manager/BookCabAuthManagerApplication.java
@@ -2,10 +2,8 @@ package com.book.cab.auth.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
 public class BookCabAuthManagerApplication {
 
 	public static void main(String[] args) {
